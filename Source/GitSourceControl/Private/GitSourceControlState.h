e3e631bee33a3cedf36e96675b09b7cafd997110
mcoms <mastercoms@tuta.io>
2023-09-15 21:46:56 -0400

    fix incorrect preprocessor

--- Source/GitSourceControl/Private/GitSourceControlState.h
+++ Source/GitSourceControl/Private/GitSourceControlState.h
@@ -142,6 +142,7 @@ public:
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FindHistoryRevision(const FString& InRevision) const override;
 #if ENGINE_MAJOR_VERSION < 5 || ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION < 3
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> GetBaseRevForMerge() const override;
+#else
 	virtual FResolveInfo GetResolveInfo() const override;
 #endif
 #if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 2
