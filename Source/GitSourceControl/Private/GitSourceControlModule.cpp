607f5d8bb474abc7f1cb61770d62efbab9390979
Damon Lewis <damon@thethreethousands.com>
2025-04-10 13:34:05 +1000

    Don't display Revert To Status Branch for maps

--- Source/GitSourceControl/Private/GitSourceControlModule.cpp
+++ Source/GitSourceControl/Private/GitSourceControlModule.cpp
@@ -200,22 +200,31 @@ void FGitSourceControlModule::CreateGitContentBrowserAssetMenu(FMenuBuilder& Men
 		FUIAction(FExecuteAction::CreateRaw( this, &FGitSourceControlModule::DiffAssetAgainstGitOriginBranch, SelectedAssets, BranchName ))
 	);
 
-	MenuBuilder.AddMenuEntry(
-		FText::Format(LOCTEXT("StatusRevert", "Revert to status branch: {0}"), FText::FromString(BranchName)),
-		FText::Format(LOCTEXT("StatusRevertDesc", "Revert this asset back to its state in status branch: {0}"), FText::FromString(BranchName)),
-#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 1
-		FSlateIcon(FAppStyle::GetAppStyleSetName(), "SourceControl.Actions.Revert"),
-#else
-		FSlateIcon(FEditorStyle::GetStyleSetName(), "SourceControl.Actions.Revert"),
-#endif
-		FUIAction(FExecuteAction::CreateLambda([SelectedAssets, BranchName]
-		{
-			const FGitSourceControlModule& GitSourceControl = FModuleManager::GetModuleChecked<FGitSourceControlModule>("GitSourceControl");
-			const FString& PathToGitBinary = GitSourceControl.AccessSettings().GetBinaryPath();
-			const FString& PathToRepositoryRoot = GitSourceControl.GetProvider().GetPathToRepositoryRoot();
-			GitSourceControlUtils::SyncAssetsFromBranch(PathToGitBinary, PathToRepositoryRoot, SelectedAssets, BranchName);
-		}))
-	);
+	const bool bAssetsContainsLevel = SelectedAssets.ContainsByPredicate([](const FAssetData& AssetData)
+	{
+		return AssetData.AssetClassPath.GetAssetName() == "World";
+	});
+	const bool bCanRevertToStatusBranch = !bAssetsContainsLevel;
+
+	if (bCanRevertToStatusBranch)
+	{
+		MenuBuilder.AddMenuEntry(
+		   FText::Format(LOCTEXT("StatusRevert", "Revert to status branch: {0}"), FText::FromString(BranchName)),
+		   FText::Format(LOCTEXT("StatusRevertDesc", "Revert this asset back to its state in status branch: {0}"), FText::FromString(BranchName)),
+   #if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 1
+		   FSlateIcon(FAppStyle::GetAppStyleSetName(), "SourceControl.Actions.Revert"),
+   #else
+		   FSlateIcon(FEditorStyle::GetStyleSetName(), "SourceControl.Actions.Revert"),
+   #endif
+		   FUIAction(FExecuteAction::CreateLambda([SelectedAssets, BranchName]
+		   {
+			   const FGitSourceControlModule& GitSourceControl = FModuleManager::GetModuleChecked<FGitSourceControlModule>("GitSourceControl");
+			   const FString& PathToGitBinary = GitSourceControl.AccessSettings().GetBinaryPath();
+			   const FString& PathToRepositoryRoot = GitSourceControl.GetProvider().GetPathToRepositoryRoot();
+			   GitSourceControlUtils::SyncAssetsFromBranch(PathToGitBinary, PathToRepositoryRoot, SelectedAssets, BranchName);
+		   }))
+	   );
+	}
 }
 
 void FGitSourceControlModule::DiffAssetAgainstGitOriginBranch(const TArray<FAssetData> SelectedAssets, FString BranchName) const
