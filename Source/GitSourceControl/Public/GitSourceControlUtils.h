eb70fa6c28059d76e423594621339dcf1f0bcaea
Damon Lewis <damon@thethreethousands.com>
2025-09-30 13:30:55 +1000

    Add descriptive comment for new function

--- Source/GitSourceControl/Public/GitSourceControlUtils.h
+++ Source/GitSourceControl/Public/GitSourceControlUtils.h
@@ -167,7 +167,8 @@ bool GetCommitInfo(const FString& InPathToGitBinary, const FString& InRepository
  */
 bool GetRemoteUrl(const FString& InPathToGitBinary, const FString& InRepositoryRoot, FString& OutRemoteUrl);
 
-TArray<FString>GetImportantGitPaths();
+// A list of core folder and file paths that we will always check locks, status, etc on startup 
+TArray<FString> GetImportantGitPaths();
 
 /**
  * Run a Git command - output is a string TArray.
